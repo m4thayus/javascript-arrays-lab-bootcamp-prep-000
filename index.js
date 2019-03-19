822c6283cf0b01e0d08491d86238d7c158a8b37b
Matt Williams <matt.seongjun@gmail.com>
2019-03-19 15:44:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-var kittens = ["Milo", "Otis", "Garfield"]
+var kittens = ["Milo", "Otis", "Garfield"];
 
-// Add your functions and code here
+function destructivelyAppenKitten(name) {
+  return 
+}
